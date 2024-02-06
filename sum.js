8113daf5bc150ca030244a65e9d0b4e00df463bf
Laura <lauraespanyol95@gmail.com>
2024-02-06 11:01:05 +0100

    Create a sum script

---
+++ sum.js
@@ -0,0 +1,10 @@
+
+console.log(5+6);
+
+// Read, Evaluate, Prit and Loop
+// It allows us to use javascript at the terminal
+// even variables and functions
+
+// Array. + tabulador x2 it will show you all the methods for an array
+// global. + tabulador x2 shows all the objects
+

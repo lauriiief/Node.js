3f368e4cd872ede0ea68cc6b44147633aee29a9e
Laura <lauraespanyol95@gmail.com>
2024-02-27 10:26:21 +0100

    Upload files

--- Express/src/controllers/planets.ts
+++ Express/src/controllers/planets.ts
@@ -70,4 +70,17 @@ const deleteById = async (req: Request, res: Response) => {
     res.status(200).json({ message: 'The planet was deleted' })
 }
 
-export {getAll, getOneById, create, updateById, deleteById}
+const createImage = async (req: Request, res: Response) => {
+  console.log(req.file);
+  const {id} = req.params;
+  const filename = req.file?.path;
+
+  if(filename){
+    db.none(`UPDATE planets SET image=$2 WHERE id=$1`, [id, filename])
+    res.status(201).json({ msg: 'Planet image upload successfully'})
+  }else{
+    res.status(400).json({ msg: 'Planet image failed to upload'})
+  }
+}
+
+export {getAll, getOneById, create, updateById, deleteById, createImage}
--- Express/src/server.ts
+++ Express/src/server.ts
@@ -2,8 +2,18 @@ import express from 'express'
 import 'express-async-errors'
 import Joi, { number, string } from 'joi'
 import morgan from 'morgan'
-import { deleteById, getAll, getOneById, updateById } from './controllers/planets'
-import { create } from 'domain'
+import { deleteById, getAll, getOneById, updateById, create, createImage } from './controllers/planets'
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+const upload = multer({ storage })
 
 const app = express()
 const port = 3000
@@ -26,6 +36,9 @@ app.put('/api/planets/:id', updateById)
 
 //DELETE /api/planets/:id: delete a planet by id, return only 200 code and a success JSON with key msg
 app.delete('/api/planets/:id', deleteById)
+
+// Set POST /planets/:id/image route for file upload (planet's image).
+app.post('api/planets/:id/image', upload.single('image'), createImage)
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
